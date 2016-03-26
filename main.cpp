b20b2fbd7a1400e24d1b560da34564833bae79f9
B10213147 <B10213147@yuntech.org.tw>
2016-03-26 13:37:53 +0100

    Find contours from the original image

--- main.cpp
+++ main.cpp
@@ -5,16 +5,7 @@ using namespace std;
 using namespace cv;
 
 int theObject[4];
-Rect objectBondingRectangle = Rect{0,0,0,0};
-
-//int to string helper function
-string intToString(int number){
-
-	//this function has a number input and string output
-	std::stringstream ss;
-	ss << number;
-	return ss.str();
-}
+Rect objectBondingRectangle;
 
 int main()
 {
@@ -22,7 +13,6 @@ int main()
     Mat differenceImage;
     Mat thresholdImage;
 
-    Mat white(500,500,CV_8UC1,Scalar(255,255,255));
     Mat img=imread("1.png");
 
     if(img.empty()){
@@ -30,9 +20,13 @@ int main()
         return -1;
     }
 
-    resize(img,img,Size(500,500));
     imshow("1.png",img);
 
+    Mat white = img.clone();
+    white.setTo(Scalar(255,255,255));
+
+    //absdiff function need two gray scalar input
+    cvtColor(white,white,CV_BGR2GRAY);
     cvtColor(img,grayImage1,CV_BGR2GRAY);
 
     absdiff(white,grayImage1,differenceImage);
@@ -49,7 +43,7 @@ int main()
     findContours(thresholdImage,contours,hierarchy,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_SIMPLE );// retrieves external contours
 
     //if contours vector is not empty, we have found some objects
-    for(int i=0; i<contours.size(); i++){
+    for(unsigned int i=0; i<contours.size(); i++){
 
         objectBondingRectangle = boundingRect(contours.at(i));
 
@@ -64,12 +58,12 @@ int main()
         theObject[2] = objx;
         theObject[3] = objy;
 
-        Mat cropedImage = Mat(img,Rect(xpos-objx/2,ypos-objy/2,objx,objy)).clone();
-        resize(cropedImage,cropedImage,Size(500,500));
+        Mat croppedImage = Mat(img,Rect(xpos-objx/2,ypos-objy/2,objx,objy)).clone();
+        resize(croppedImage,croppedImage,Size(300,300));
 
-        char imageName[6] = "test";
+        char imageName[10] = "test";
         imageName[4] = i+48;
-        imshow(imageName,cropedImage);
+        imshow(imageName,croppedImage);
     }
 
     waitKey(0);
